ffef6b3aea0fad72a2a1c88172d1cacfcf620226
Rin-t <rin.t.4921@gmail.com>
2023-04-10 22:58:29 +0900

    Initial Commit

---
+++ PokemonDexWithSwiftUI/PokemonDexWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDexWithSwiftUIApp.swift
+//  PokemonDexWithSwiftUI
+//
+//  Created by Rin on 2023/04/10.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDexWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
