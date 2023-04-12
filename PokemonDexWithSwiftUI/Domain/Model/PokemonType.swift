f69a9bd6d96dfd0db080b57f9b2e648a34362d84
Rin-t <rin.t.4921@gmail.com>
2023-04-12 22:49:39 +0900

    TypeのEnumをフォーマット

--- PokemonDexWithSwiftUI/Domain/Model/PokemonType.swift
+++ PokemonDexWithSwiftUI/Domain/Model/PokemonType.swift
@@ -7,23 +7,24 @@
 
 import Foundation
 
-enum PokemonType {
-  case normal
-  case fire
-  case water
-  case electric
-  case grass
-  case ice
-  case fighting
-  case poison
-  case ground
-  case flying
-  case psychic
-  case bug
-  case rock
-  case ghost
-  case dragon
-  case dark
-  case steel
-  case fairy
+enum PokemonType: String {
+    case normal
+    case fire
+    case water
+    case electric
+    case grass
+    case ice
+    case fighting
+    case poison
+    case ground
+    case flying
+    case psychic
+    case bug
+    case rock
+    case ghost
+    case dragon
+    case dark
+    case steel
+    case fairy
+    case unknown // タイプの中にunknownあるの変か？
 }
