a329ad5523e4d273121b121c5701c618cb934b66
Rin-t <rin.t.4921@gmail.com>
2023-04-12 21:32:19 +0900

    Presentation層で使用するためのModelを作成

---
+++ PokemonDexWithSwiftUI/Domain/Model/PokemonModel.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonModel.swift
+//  PokemonDexWithSwiftUI
+//
+//  Created by Rin on 2023/04/12.
+//
+
+import Foundation
+
+struct PokemonModel {
+    let name: String
+    let id: Int
+    let frontImage: String
+    let shinyImage: String
+    let type1: PokemonType
+    let type2: PokemonType?
+}
---
+++ PokemonDexWithSwiftUI/Domain/Model/PokemonType.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonTyoe.swift
+//  PokemonDexWithSwiftUI
+//
+//  Created by Rin on 2023/04/12.
+//
+
+import Foundation
+
+enum PokemonType {
+  case normal
+  case fire
+  case water
+  case electric
+  case grass
+  case ice
+  case fighting
+  case poison
+  case ground
+  case flying
+  case psychic
+  case bug
+  case rock
+  case ghost
+  case dragon
+  case dark
+  case steel
+  case fairy
+}
