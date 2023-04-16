53a20ddb39f9bd7c2c3e7eab9cc33d4bf914653b
Rin-t <rin.t.4921@gmail.com>
2023-04-16 12:19:22 +0900

    HomeViewModelを作成

---
+++ PokemonDexWithSwiftUI/Presentaion/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  HomeViewModel.swift
+//  PokemonDexWithSwiftUI
+//
+//  Created by Rin on 2023/04/11.
+//
+
+import Foundation
+import SwiftUI
+
+protocol HomeViewModelInput {
+    func onAppear()
+    func tappeGrid(index: Int)
+}
+
+protocol HomeViewModelOutput {
+    var isPresentDetailVC: Bool { get }
+    var isShowingFailToPokemonAlert: Bool { get }
+    var pokemon: PokemonModel? { get }
+    var pokemons: [PokemonModel] { get }
+}
+
+protocol HomeViewModelType {
+    var input: HomeViewModelInput { get }
+    var output: HomeViewModelOutput { get }
+}
+
+final class HomeViewModel: ObservableObject, HomeViewModelInput, HomeViewModelOutput {
+
+    @Published var isPresentDetailVC: Bool = false
+    @Published var isShowingFailToPokemonAlert: Bool = false
+    @Published var pokemons: [PokemonModel] = []
+    @Published var pokemon: PokemonModel?
+
+    private let useCase: PokemonUseCaseProtocol
+
+    init(useCase: PokemonUseCaseProtocol = PokemonUseCase()) {
+        self.useCase = useCase
+    }
+
+    func onAppear() {
+        Task {
+            do {
+                let startSearchId = PokemonId(value: 1)
+                let endSearchId = PokemonId(value: 151)
+                pokemons = try await useCase.fetchPokemonList(from: startSearchId, to: endSearchId)
+            } catch {
+                isShowingFailToPokemonAlert = true
+            }
+        }
+    }
+
+    func tappeGrid(index: Int) {
+        pokemon = pokemons[index]
+        
+        guard let _ = pokemon else {
+            print("poekemonがnil")
+            return
+        }
+        isPresentDetailVC = true
+    }
+}
+
+
+extension HomeViewModel: HomeViewModelType {
+
+    var input: HomeViewModelInput { return self }
+    var output: HomeViewModelOutput { return self }
+}
