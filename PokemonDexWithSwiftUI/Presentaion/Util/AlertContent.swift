598a99f528ab990e99e74f236abcd3dc6fc36c6b
Rin-t <rin.t.4921@gmail.com>
2023-04-17 21:29:40 +0900

    Alertの表示内容用structを作成

---
+++ PokemonDexWithSwiftUI/Presentaion/Util/AlertContent.swift
@@ -0,0 +1,14 @@
+//
+//  AlertContent.swift
+//  PokemonDexWithSwiftUI
+//
+//  Created by Rin on 2023/04/17.
+//
+
+import Foundation
+
+struct AlertContent {
+    let title: String
+    let message: String
+    let actionText: String
+}
