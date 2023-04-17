d81074941ab8424c1428feca90a43b70df0f975c
Rin-t <rin.t.4921@gmail.com>
2023-04-17 22:36:57 +0900

    PokemonDetailViewModelの処理を追加

--- PokemonDexWithSwiftUI/Presentaion/Detail/ViewModel/PoekemonDetailViewModel.swift
+++ PokemonDexWithSwiftUI/Presentaion/Detail/ViewModel/PoekemonDetailViewModel.swift
@@ -5,4 +5,23 @@
 //  Created by Rin on 2023/04/17.
 //
 
-import Foundation
+import SwiftUI
+
+final class PokemonDetailViewModel: ObservableObject {
+
+    @Published var pokemon: PokemonModel
+    @Published var selectedImageUrl: URL!
+
+    init(pokemon: PokemonModel) {
+        self.pokemon = pokemon
+        self.selectedImageUrl = URL(string: pokemon.frontImage)!
+    }
+
+    func tappedNormalButton() {
+        selectedImageUrl = URL(string: pokemon.frontImage)!
+    }
+
+    func tappedShinyButton() {
+        selectedImageUrl = URL(string: pokemon.shinyImage)!
+    }
+}
